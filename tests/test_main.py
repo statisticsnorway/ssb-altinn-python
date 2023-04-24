461f1b72a16075caa6d15b3827e027bf36de7c46
Bruer-Skarsbø, Øyvind <obr@ssb.no>
2023-04-24 11:18:46 +0200

    Fixed sorting of libraries

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
 """This module contains the tests for the __main__.py."""
 
-import os
 import importlib.util
+import os
 from unittest.mock import patch
+
 import altinn.__main__ as main_module
+
 
 def test_main_direct_call():
     """Test that main() is called when altinn.__main__.main() is called."""
@@ -11,10 +13,15 @@ def test_main_direct_call():
         main_module.main()
         mock_main.assert_called_once()
 
+
 def test_main_script_execution():
     """Test that main() is called when __main__.py is run as a script."""
     with patch("altinn.file.main") as mock_main:
-        script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src", "altinn", "__main__.py"))
+        script_path = os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), "..", "src", "altinn", "__main__.py"
+            )
+        )
         spec = importlib.util.spec_from_file_location("__main__", script_path)
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
