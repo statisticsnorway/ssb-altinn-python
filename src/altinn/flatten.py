1c62fc2c5c5a1744772ea05ac9ae62e9d97ebb61
Øyvind Bruer-Skarsbø <obr@ssb.no>
2023-07-10 15:45:53 +0200

    Added angiver-id to ISEE dataframe

--- src/altinn/flatten.py
+++ src/altinn/flatten.py
@@ -6,9 +6,12 @@ generic functions that should support all types of xml-forms. It requires
 the user to specify how to recode old fieldnames of Altinn2 to the new names
 of Altinn3. This is done in a separate file.
 """
+import pandas as pd
+
+from .parser import ParseSingleXml
 
 
-def isee_transform(df, mapping=None):
+def isee_transform(file_path, mapping=None):
     """Transforms dataframe to ISEE-format.
 
     Transforms the given DataFrame by selecting certain columns and renaming
@@ -16,8 +19,7 @@ def isee_transform(df, mapping=None):
     to the correct ISEE variable names.
 
     Args:
-        df (pandas.DataFrame): A DataFrame containing flattened data extracted
-            from an altinn3 XML file.
+        file_path (str): The path to the XML file.
         mapping (dict): The mapping dictionary to map variable names in the
             'feltnavn' column. The default value is an empty dictionary
             (if mapping is not needed).
@@ -29,26 +31,21 @@ def isee_transform(df, mapping=None):
     if mapping is None:
         mapping = {}
 
-    df = df.rename(
-        columns={
-            "raNummer": "Skjema_id",
-            "delregNr": "Delreg_nr",
-            "enhetsIdent": "Ident_nr",
-            "enhetsType": "Enhets_type",
-            "skjemaVersjon": "version_nr",
-        }
-    )[
-        [
-            "Skjema_id",
-            "Delreg_nr",
-            "Ident_nr",
-            "Enhets_type",
-            "feltnavn",
-            "feltverdi",
-            "version_nr",
-        ]
-    ]
+    df = ParseSingleXml.to_dataframe(file_path)
 
-    df["feltnavn"] = df["feltnavn"].replace(mapping, inplace=False)
+    def extract_angiver_id():
+        """Extracts the text after "/form_" and before ".xml" in string."""
+    
+        start_index = file_path.find("/form_") + len("/form_")
+        end_index = file_path.find(".xml", start_index)
+        if start_index != -1 and end_index != -1:
+            extracted_text = file_path[start_index:end_index]
+            return extracted_text
+        else:
+            return None
+    
+    angiver_id = extract_angiver_id()
+
+    df = df.assign(angiver_id=angiver_id)
 
     return df
