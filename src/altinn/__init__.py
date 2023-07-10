5c5045d6d234833268f879975e1a4aa46bb2c271
Øyvind Bruer-Skarsbø <obr@ssb.no>
2023-07-10 17:48:19 +0200

    ADded isee_flatten to __init__

--- src/altinn/__init__.py
+++ src/altinn/__init__.py
@@ -1,7 +1,8 @@
 """SSB Altinn Python."""
 
 from .file import FileInfo
+from .flatten import isee_transform
 from .parser import ParseSingleXml
 
 
-__all__ = ["FileInfo", "ParseSingleXml"]
+__all__ = ["FileInfo", "ParseSingleXml", "isee_transform"]
