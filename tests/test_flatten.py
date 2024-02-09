50d7ee41aed902b69e0cf275aaaf916bb0a8b415
Vidar Strandseter <vst@ssb.no>
2024-02-09 10:49:27 +0100

    Added test for xml_transform

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from altinn.flatten import isee_transform
+from altinn.flatten import xml_transform
 
 
 def test_isee_transform() -> None:
@@ -10,3 +11,11 @@ def test_isee_transform() -> None:
     print(len(df))
 
     assert len(df) == 61
+
+def test_xml_transform() -> None:
+    xml_file = Path(__file__).parent / "data" / "form_373a35bb8808.xml"
+    df = xml_transform(str(xml_file))
+    print(df.head())
+    print(len(df))
+
+    assert len(df) == 107
