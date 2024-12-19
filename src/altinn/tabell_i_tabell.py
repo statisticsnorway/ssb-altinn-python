b32d9608c71e225856cc87198b6afccc78f63239
ssb-dor <dor@ssb.no>
2024-12-19 08:17:45 +0000

    First draft of flattening a 'table-in-table' object

---
+++ src/altinn/tabell_i_tabell.py
@@ -0,0 +1,150 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %%
+import logging
+
+import numpy as np
+import pandas as pd
+
+import altinn as alt
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s",
+    # datefmt='%Y-%m-%d %H:%M:%S',
+    encoding="utf-8",
+)
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+
+
+# %%
+def flatten_tabell_i_tabell(
+    table_data: pd.DataFrame,
+    rest_of_data: pd.DataFrame,
+    tabell_felter: list,
+    rad_felter: list,
+    counter_starting_value: int = 1,
+):
+    """Args:
+        table_data (pd.DataFrame):
+        rest_of_data (pd.DataFrame):
+        counter_starting_value (int): Sets the lopenr to this value for the first part of the table object. If there are multiple table objects that belong in the same dynamic list, adjust this value to avoid duplicate FELTNAVN.
+
+    Notes:
+        A simple way to split data is using .loc to filter out relevant rows to a new dataframe, and then using the index to remove them from the primary dataset.
+
+    Example:
+            table_data = df.loc[df[FELTNAVN].str.startswith("tabell_prefix")]
+            rest_of_data = df.drop(table_data.index, axis=0)
+            assert df.shape[0] == table_data.shape[0]+rest_of_data.shape[0]
+    """
+    table_data = table_data[["FELTNAVN", "FELTVERDI"]]
+    pattern = "|".join(tabell_felter)
+    df_meta = table_data[
+        table_data["FELTNAVN"].str.contains(pattern, case=False, na=False)
+    ]
+    pattern = "|".join(rad_felter)
+    df_row = table_data[
+        table_data["FELTNAVN"].str.contains(pattern, case=False, na=False)
+    ]
+    relevante_lopenr = list(df_row["FELTNAVN"].str.split("_").str[-1].unique())
+    df_meta = df_meta.loc[
+        df_meta["FELTNAVN"].str.split("_").str[-1].isin(relevante_lopenr)
+    ]
+    flattened_table_data: pd.DataFrame = (
+        pd.DataFrame()
+    )  # Dataframe to be filled with flattened data.
+    for lopenr in relevante_lopenr:
+        row_data: dict = {}
+        for felt in rad_felter:
+            row_data[felt] = tuple(
+                df_row.loc[
+                    (df_row["FELTNAVN"].str.endswith(lopenr))
+                    & (df_row["FELTNAVN"].str.contains(felt))
+                ]["FELTVERDI"].values
+            )
+        max_length = max(len(values) for values in row_data.values())
+        test = pd.DataFrame(  # TODO: Må kanskje endres
+            {
+                key: list(values) + [np.nan] * (max_length - len(values))
+                for key, values in row_data.items()
+            }
+        )
+        table_data: dict = {}
+        for felt in tabell_felter:
+            logger.debug(f"df_meta for felt {felt}: \n{df_meta}")
+            verdi = df_meta.loc[
+                (df_meta["FELTNAVN"].str.endswith(lopenr))
+                & (df_meta["FELTNAVN"].str.contains(felt))
+            ]["FELTVERDI"].item()
+            test[felt] = verdi
+
+        current_count = (
+            max(flattened_table_data["FELTNAVN"].str[-3:].astype(int)) + 1
+            if "FELTNAVN" in flattened_table_data.columns
+            else counter_starting_value
+        )  # Finds the current number of rows in the dynamic list so the counting can continue.
+        dynamic_list_rows = (
+            test.reset_index()
+            .assign(
+                lopenr=lambda x: range(current_count, current_count + len(test))
+            )  # This needs to be changed to count upwards for each row that exists, so they are not overwritten/become duplicates.
+            .drop(columns="index")
+            .melt(id_vars="lopenr", value_name="FELTVERDI")
+            .assign(
+                FELTNAVN=lambda x: x["variable"]
+                + "_"
+                + x["lopenr"].astype(str).str.zfill(3)
+            )[["FELTNAVN", "FELTVERDI"]]
+        )
+
+        flattened_table_data = pd.concat([flattened_table_data, dynamic_list_rows])
+
+    # Re-adding form metadata to flattened_table_data
+    skjema_id = list(rest_of_data["SKJEMA_ID"].unique())[0]
+    delreg_nr = list(rest_of_data["DELREG_NR"].unique())[0]
+    ident_nr = list(rest_of_data["IDENT_NR"].unique())[0]
+    enhets_type = list(rest_of_data["ENHETS_TYPE"].unique())[0]
+    version_nr = list(rest_of_data["VERSION_NR"].unique())[0]
+    flattened_table_data = (
+        flattened_table_data.assign(SKJEMA_ID=skjema_id)
+        .assign(DELREG_NR=delreg_nr)
+        .assign(IDENT_NR=ident_nr)
+        .assign(ENHETS_TYPE=enhets_type)
+        .assign(VERSION_NR=version_nr)
+    )
+
+    return pd.concat([rest_of_data, flattened_table_data]).reset_index(drop=True)
+
+
+# %%
+source_file = "gs://ssb-primaer-j-skjema-data-kilde-prod/lbrund/altinn/2024/12/12/0f461103b8e1_5da2a159-2561-4e17-be01-a150b2e6c8bd/form_0f461103b8e1.xml"
+
+# %%
+df = alt.isee_transform(source_file)
+
+table_data = df.loc[df["FELTNAVN"].str.startswith("AnVekstMineralGjodsGroup_")]
+rest_of_data = df.drop(table_data.index, axis=0)
+assert df.shape[0] == table_data.shape[0] + rest_of_data.shape[0]
+
+output = flatten_tabell_i_tabell(
+    table_data=table_data,
+    rest_of_data=rest_of_data,
+    tabell_felter=["Navn"],
+    rad_felter=["Aarstid", "Areal", "Mengde"],
+)
