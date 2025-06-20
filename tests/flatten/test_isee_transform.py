6284b804014e31453d7824a1c396a879ea3ddcd5
Vidar Strandseter <vst@ssb.no>
2025-06-20 08:19:58 +0200

    added tests for tag_list and checkboxses

--- tests/flatten/test_isee_transform.py
+++ tests/flatten/test_isee_transform.py
@@ -10,3 +10,18 @@ def test_isee_transform() -> None:
     print(len(df))
 
     assert len(df) == 63
+
+
+def test_isee_transform_tags_checkbox() -> None:
+    xml_file = Path(__file__).parent / "data" / "form_1e7d1c3e69c0.xml"
+
+    tags = ["SkjemaData", "Kontakt"]
+    checkboxList = ["kontraktType"]
+
+    df = isee_transform(
+        str(xml_file), tag_list=tags, checkbox_vars=checkboxList, unique_code=False
+    )
+    print(df.tail(10))
+    print(len(df))
+
+    assert len(df) == 23
