1acbd1c5a9acbbc476d259b10aacf8b72f502dfb
ssb-vst <vst@ssb.no>
2026-06-02 14:27:17 +0200

    change order of python-versions

--- noxfile.py
+++ noxfile.py
@@ -23,7 +23,7 @@ except ImportError:
     raise SystemExit(dedent(message)) from None
 
 package = "altinn"
-python_versions = ["3.13", "3.12", "3.14"]
+python_versions = ["3.12", "3.13", "3.14"]
 python_versions_for_test = python_versions
 nox.needs_version = ">= 2025.2.9"
 nox.options.sessions = (
